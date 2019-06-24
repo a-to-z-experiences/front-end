60bfa224e39c72c2ef2e165e07a53bd9d2d0c531
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-25 07:31:41 +1000

    finalized importing the dependencies within src index js

--- experiences/src/index.js
+++ experiences/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-// import createStore so we can create our global store
-import { createStore } from "react-redux";
-// import Router so App can use match location history I think?
+// importing rootReducer so our store variable that we pass to Provider as props can use the reducer's returned state
+import { rootReducer } from "./reducers";
+// importing createStore so we can create our global store, applyMiddleware is a method so we can use loggers and thunks, compose so we can use the redux tools
+import { createStore, applyMiddleware, compose } from "redux";
+// importing Provider so we can pass our store prop into it that'll give our inner components access to it
+import { Provider } from "react-redux";
+// importing Router so App can use match location history I think?
 import { Router } from "react-router-dom";
-
-// create const store prop that links our store with our reducer
-const store = createStore(rootReducer);
+// importing thunk so we can use the dispatch method manually within our thunk function that gets returned in our action creators
+import thunk from "redux-thunk";
+// importing logger so we can see the actions being dispatched and whether our calls were successful or not
+import logger from "redux-logger";
+// creating composeEnhancers variable that allows us to use redux tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// create const store prop that links our store with our reducer, and links the middleware methods which intervene between action creators and actions like thunk and logger?
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>

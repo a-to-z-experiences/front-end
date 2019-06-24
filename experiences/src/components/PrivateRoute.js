f94f0dfb9b71c9bef99e8b5b8cc1c5d7abcb596c
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-25 09:09:05 +1000

    copied and pasted in a template for privateroute

--- experiences/src/components/PrivateRoute.js
+++ experiences/src/components/PrivateRoute.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-export default class PrivateRoute extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
         )
-    }
-}
+      }
+    />
+  );
+};
+
+export default PrivateRoute;

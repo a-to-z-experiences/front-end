7780a286df9507602e0aa403f518dc0f22d8bdb4
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-25 07:58:18 +1000

    created a basic version of all components, defined routes and paths for them within app js, created privateroute component

--- experiences/src/App.js
+++ experiences/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+// importing route so we can direct a route for a specific url
 import { Route } from "react-router-dom";
+// importing PrivateRoute which displays another component conditionally on the token in localStorage
+import PrivateRoute from "./components/PrivateRoute";
+// importing UserHome
+import UserHome from "./components/UserHome";
+// importing Login which is the conditional component being displayed by PrivateRoute
+import Login from "./components/Login";
+// importing AvailableExperiences which contain all the experiences inside the data.experiences array
+import AvailableExperiences from "./components/AvailableExperiences";
+// css file at end
 import "./App.css";
 
 function App() {
-  return ( 
+  return (
     <div className="App">
-      <Route exact path="/"/>
+      <PrivateRoute exact path="/" component={UserHome} />
+      <Route path="/login" component={Login} />
+      <Route path="/experiences" component={AvailableExperiences} />
     </div>
   );
 }

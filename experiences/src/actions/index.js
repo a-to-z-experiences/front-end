352d3460f362b9b411413df5010686ecab62397e
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-25 07:39:00 +1000

    created a basic action creator that returns a thunk, created action type vars

---
+++ experiences/src/actions/index.js
@@ -0,0 +1,13 @@
+// creating getData variables for action types
+export const GET_USER_DATA_START = "GET_USER_DATA_START";
+export const GET_USER_DATA_SUCCESS = "GET_USER_DATA_SUCCESS";
+export const GET_USER_DATA_FAILURE = "GET_USER_DATA_FAILURE";
+// creating getExperiencesData variables for action types
+export const GET_EXPERIENCES_DATA_START = "GET_EXPERIENCES_DATA_START";
+export const GET_EXPERIENCES_DATA_SUCCESS = "GET_EXPERIENCES_DATA_SUCCESS";
+export const GET_EXPERIENCES_DATA_FAILURE = "GET_EXPERIENCES_DATA_FAILURE";
+// creating getUserData action creator
+export const getUserData = () => dispatch => {};
+
+// creating getExperiencesData action creator
+export const getExperiencesData = () => dispatch => {};

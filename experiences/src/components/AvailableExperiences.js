ff30c7cb030c1dcd26def39288febecc886f0530
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-26 02:17:35 +1000

    built out skeleton of availableexperiences component

--- experiences/src/components/AvailableExperiences.js
+++ experiences/src/components/AvailableExperiences.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+// import getExperiencesData fn to allow us to match events with action objects that change the reducer's state that changes what we receive as props
+import { getExperiencesData } from "../actions";
 // import connect to connect the action creators and props we want from reducer's state to component
 import { connect } from "react-redux";
 
 class AvailableExperiences extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {};
+  componentDidMount() {
+    getExperiencesData();
+  }
+  render() {
+    return <div className="available-experiences" />;
+  }
 }
 
 // creating mapStateToProps fn that takes in state from reducers. We pass props to Login by utilizing the reducer's state
 const mapStateToProps = state => {
   return {
-    error: state.error
+    error: state.error,
+    availableExperiencesArray: state.AvailableExperiences
   };
 };
 // linking mapStateToProps, action creators to Login component
 export default connect(
   mapStateToProps,
-  { login, register }
+  { getExperiencesData }
 )(AvailableExperiences);
